7f539c03860492f726e7e61ddc09937635910d43
Rémy Frenoy <rfrenoy@gmail.com>
2020-12-05 08:51:16 +0100

    using binary computation to compute seat value for day 5 solution

--- day5.py
+++ day5.py
@@ -1,24 +1,12 @@
 import numpy as np
 import math
 
-def divide(values, front_or_back):
-    if front_or_back in ['L', 'F']:
-        return values[:math.ceil(int(len(values)/2))]
-    elif front_or_back in ['R', 'B']:
-        return values[int(len(values)/2):]
-
 def which_seat(vals):
-    nb_values = 128 if vals[0] in ['F', 'B'] else 8
-    seat_ids = np.arange(nb_values)
-    for val in vals:
-        seat_ids = divide(seat_ids, val)
-    return seat_ids
+    s = vals.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1')
+    return int(f'0b{s}', 2)
 
 def seat_from_str(s):
-    row = which_seat(s[:7])[0]
-    column = which_seat(s[7:])[0]
-    return row * 8 + column
-
+    return which_seat(s[:7]) * 8 + which_seat(s[7:])
 
 if __name__ == '__main__':
     with open('data/day5.txt', 'r') as in_f:
